1d1a540c8d8c49cdb54867f4b4c81ca294c35745
ariicodes <arielaisrael12@gmail.com>
2024-08-26 03:07:26 -0400

    resolvers updated

--- server/src/graphql/resolvers.ts
+++ server/src/graphql/resolvers.ts
@@ -1,5 +1,33 @@
+import { Event, UserInput } from '../types';
+import {
+	getEvents,
+	getEventById,
+	createRegistration,
+} from '../services/eventService';
+import { sendSMS } from '../utils/twilioClient';
+
 export const resolvers = {
 	Query: {
-		hello: () => 'Hello world!',
+		events: async () => {
+			return await getEvents();
+		},
+		event: async (_: any, { id }: { id: string }) => {
+			return await getEventById(id);
+		},
+	},
+	Mutation: {
+		registerForEvent: async (
+			_: any,
+			{ eventId, userData }: { eventId: string; userData: UserInput }
+		) => {
+			const success = await createRegistration(eventId, userData);
+			if (success) {
+				await sendSMS(
+					userData.phone,
+					`You have successfully registered for the event!`
+				);
+			}
+			return success;
+		},
 	},
 };
